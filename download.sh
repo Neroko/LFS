1baa96155d97cdff3d7dcb6be5728f77f804f948
TJ <tj.anscombe@gmail.com>
2025-03-15 18:42:19 -0400

    modified:   download.sh

--- download.sh
+++ download.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 # Download LFS Script Downloader
-wget 
+
+linux_net_link="https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/debian-12.10.0-amd64-netinst.iso"
+linux_dvd_link="https://cdimage.debian.org/debian-cd/current/amd64/iso-dvd/debian-12.10.0-amd64-DVD-1.iso"
+output_file="lfs-downloader.sh"
+wget_tries="3"
+
+wget -O "$output_file" -nc -t "$wget_tries" -c "$linux_net_link"
+
+wget                                    \
+    --output-file="$output_file"        \
+    --no-clobber                        \
+    --tries="$wget_tries"               \
+    --continue                          \
+    "$linux_net_link"
