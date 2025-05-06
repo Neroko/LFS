50c66742fbb738bfa099a917730c043cbe9f727f
TJ <tj.anscombe@gmail.com>
2025-05-05 21:29:14 -0400

    Update 00-version-check.sh

--- scripts/lfsdownloader/00-version-check.sh
+++ scripts/lfsdownloader/00-version-check.sh
@@ -6,7 +6,7 @@ display_title="== Linux From Scratch (LFS) Versions Checks =="
 current_version="12.3"
 #
 # VERSION (SCRIPT):
-script_version="1.0.0.2"
+script_version="1.0.0.3"
 #
 # DATE LAST EDITED:
 #   05/05/2025
