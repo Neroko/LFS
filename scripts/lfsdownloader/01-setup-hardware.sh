1c7249a8065221b7590c110364ad9804ebf9f637
TJ <tj.anscombe@gmail.com>
2025-05-06 12:50:26 -0400

    Update 01-setup-hardware.sh

--- scripts/lfsdownloader/01-setup-hardware.sh
+++ scripts/lfsdownloader/01-setup-hardware.sh
@@ -64,9 +64,67 @@ script_version="1.0.0.0"
 #   to building an LFS system is to use an available empty partition or, if you have enough unpartitioned space, to
 #   create one.
 
+#   A minimal system requires a partition of around 10 gigabytes (GB). This is enough to store all the source
+#   tarballs and compile the packages. However, if the LFS system is intended to be the pimary Linux system,
+#   additional software will probably be installed which will require additional space. A 30 GB partition is a
+#   reasonable size to provide for growth. The LFS system itself will not take up this much room. A large portion
+#   of this requirement is to provide sufficient free temporary storage as well as for adding additional capabilities
+#   after LFS is complete. Additionally, compiling packages can require a lot of disk space which will be reclaimed
+#   after the package is installed.
 
+#   Because there is not always enough Random Access Memory (RAM) available for compilation processes, it is a good
+#   idea to use a small disk partition as 'swap' space. This is used by the kernel to store seldom-used data and
+#   leave more memory available for active processes. Ths 'swap' partition for an LFS system can be the same as the
+#   one used by the host system, in which case it is not necessary to create another one.
 
+#   Start a disk partitioning program such as 'cfdisk' or 'fdisk' with a command line option naming the hard disk on
+#   which the new partition will be created - for example '/dev/sda' for the primary disk drive. Create a Linux
+#   native partition and a 'swap' partition, if needed. Please refer to 'cfdisk(8)' or 'fdisk(8)' if you do not yet
+#   know how to use the programs.
 
+# -- NOTE --
+# For experienced users, other partitioning schemes are possible. The new LFS system can be on a software RAID array
+# or an LVM logical volume. However, some of these options require an 'initramfs'. which is an advanced topic. These
+# partitioning methodologies are not recommended for first time LFS users.
+
+#   Remember the designation of the new partition (e.g., sda5). This script will refer to this as the LFS partition.
+#   Also remember the designation of the 'swap' partition. These names will be needed later for the '/etc/fstab' file.
+
+# = 2.4.1. Other Partition Issues
+#   Requests for advice on system partitioning are often posted on the LFS mailing lists. This is a highly subjective
+#   topic. The default for most distributions is to use the entire drive with the exception of one small swap partition.
+#   This is not optimal for LFS for several reasons. It reduces flexibility, makes sharing of data across multiple
+#   distributions or LFS builds more difficult, makes backups more time consuming, and can waste disk space through
+#   inefficient allocation of file system structures.
+
+# = 2.4.1.1. The Root Partition
+#   A root LFS partition (not to be confused with the /root directory) of twenty gigabytes is a good compromise for
+#   most systems. It provides enough space to build LFS and most of BLFS, but is small enough so that multiple
+#   partitions can be easily created for experimentation.
+
+# = 2.4.1.2. The Swap Partition
+#   Most distributions automatically create a swap partition. Generally the recommended size of the swap partition is
+#   about twice the amount of physical RAM, however this is rarely needed. If disk space is limited, hold the swap
+#   partition to two gigabytes and monitor the amount of disk swapping. If you want to use the hibernation feature
+#   (suspend-to-disk) of Linux, it writes out the contents of RAM to the swap partition before turning off the machine.
+#   In this case the size of the swap partition should be at least as large as the system's installed RAM.
+#   Swapping is never good. For mechanical hard drives you can generally tell if a system is swapping by just listening
+#   to disk activity and observing how the system reacts to commands. With an SSD you will not be able to hear swapping,
+#   but you can tell how much swap space is being used by running the top or free programs. Use of an SSD for a swap
+#   partition should be avoided if possible. The first reaction to swapping should be to check for an unreasonable
+#   command such as trying to edit a five gigabyte file. If swapping becomes a normal occurrence, the best solution is
+#   to purchase more RAM for your system.
+
+# = 2.4.1.3. The Grub Bios Partition
+#   If the boot disk has been partitioned with a GUID Partition Table (GPT), then a small, typically 1 MB, partition
+#   must be created if it does not already exist. This partition is not formatted, but must be available for GRUB to
+#   use during installation of the boot loader. This partition will normally be labeled 'BIOS Boot' if using fdisk or
+#   have a code of EF02 if using the gdisk command.
+
+# -- Note --
+# The Grub Bios partition must be on the drive that the BIOS uses to boot the system. This is not necessarily
+# the drive that holds the LFS root partition. The disks on a system may use different partition table types. The
+# necessity of the Grub Bios partition depends only on the partition table type of the boot disk.
 
 
 
