c76a67ee6421060d17b8474fe2f6eaaf8108a6ec
TJ <tj.anscombe@gmail.com>
2025-05-06 14:24:24 -0400

    Update 01-setup-hardware.sh

--- scripts/lfsdownloader/01-setup-hardware.sh
+++ scripts/lfsdownloader/01-setup-hardware.sh
@@ -155,34 +155,37 @@ script_version="1.0.0.0"
 #   file. Details about how to specify partitions will be discussed in Section 10.2, “Creating the /etc/fstab File”.
 
 # == 2.5. Creating a File System on the Partition
-# A partition is just a range of sectors on a disk drive, delimited by boundaries set in a partition table. Before the operating
-# system can use a partition to store any files, the partition must be formatted to contain a file system, typically consisting
-# of a label, directory blocks, data blocks, and an indexing scheme to locate a particular file on demand. The file system
-# also helps the OS keep track of free space on the partition, reserve the needed sectors when a new file is created or an
-# existing file is extended, and recycle the free data segments created when files are deleted. It may also provide support
-# for data redundancy, and for error recovery.
-# LFS can use any file system recognized by the Linux kernel, but the most common types are ext3 and ext4. The choice
-# of the right file system can be complex; it depends on the characteristics of the files and the size of the partition. For
-# example:
+#     A partition is just a range of sectors on a disk drive, delimited by boundaries set in a partition table. Before the
+#     operating system can use a partition to store any files, the partition must be formatted to contain a file system,
+#     typically consisting of a label, directory blocks, data blocks, and an indexing scheme to locate a particular file
+#     on demand. The file system also helps the OS keep track of free space on the partition, reserve the needed sectors
+#     when a new file is created or an existing file is extended, and recycle the free data segments created when files
+#     are deleted. It may also provide support for data redundancy, and for error recovery. 
 
-# ext2
-# is suitable for small partitions that are updated infrequently such as /boot.
-# ext3
-# is an upgrade to ext2 that includes a journal to help recover the partition's status in the case of an unclean shutdown.
-# It is commonly used as a general purpose file system.
-# ext4
-# is the latest version of the ext family of file systems. It provides several new capabilities including nano-second
-# timestamps, creation and use of very large files (up to 16 TB), and speed improvements.
-# Other file systems, including FAT32, NTFS, JFS, and XFS are useful for specialized purposes. More information about
-# these file systems, and many others, can be found at https://en.wikipedia.org/wiki/Comparison_of_file_systems.
-# LFS assumes that the root file system (/) is of type ext4. To create an ext4 file system on the LFS partition, issue the
-# following command:
-# mkfs -v -t ext4 /dev/<xxx>
-# Replace <xxx> with the name of the LFS partition.
-# If you are using an existing swap partition, there is no need to format it. If a new swap partition was created, it will
-# need to be initialized with this command:
-# mkswap /dev/<yyy>
-# Replace <yyy> with the name of the swap partition.
+#     LFS can use any file system recognized by the Linux kernel, but the most common types are ext3 and ext4. The choice
+#     of the right file system can be complex; it depends on the characteristics of the files and the size of the partition.
+#     For example:
+
+#       ext2
+#         is suitable for small partitions that are updated infrequently such as /boot.
+#       ext3
+#         is an upgrade to ext2 that includes a journal to help recover the partition's status in the case of an unclean shutdown.
+#         It is commonly used as a general purpose file system.
+#       ext4
+#         is the latest version of the ext family of file systems. It provides several new capabilities including nano-second
+#         timestamps, creation and use of very large files (up to 16 TB), and speed improvements.
+#     Other file systems, including FAT32, NTFS, JFS, and XFS are useful for specialized purposes. More information about
+#     these file systems, and many others, can be found at https://en.wikipedia.org/wiki/Comparison_of_file_systems.
+
+#     LFS assumes that the root file system (/) is of type ext4. To create an ext4 file system on the LFS partition, issue the
+#     following command:
+#mkfs -v -t ext4 /dev/<xxx>
+
+#     Replace <xxx> with the name of the LFS partition.
+#     If you are using an existing swap partition, there is no need to format it. If a new swap partition was created, it will
+#     need to be initialized with this command:
+#mkswap /dev/<yyy>
+#   Replace <yyy> with the name of the swap partition.
 
 
 
