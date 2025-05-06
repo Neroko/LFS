696d1d8f56a60bdb132d093de9c36ba34049f93a
TJ <tj.anscombe@gmail.com>
2025-05-06 11:23:33 -0400

    Update 01-setup-hardware.sh

--- scripts/lfsdownloader/01-setup-hardware.sh
+++ scripts/lfsdownloader/01-setup-hardware.sh
@@ -1,2 +1,33 @@
 #!/bin/bash
 
+display_title="== Linux From Scratch (LFS) Setup System =="
+#
+# VERSION (LFS):
+current_version="12.3"
+#
+# VERSION (SCRIPT):
+script_version="1.0.0.0"
+#
+# DATE LAST EDITED:
+#   05/06/2025
+#
+# DATE CREATED:
+#   05/06/2025
+#
+# AUTHOR:
+#   TerryJohn Anscombe
+#
+# DESCRIPTION
+#   Script to list version numbers of critical development tools
+#
+# USAGE:
+#   version-check.sh [options] ARG1
+#
+# OPTIONS:
+#   -h, --help                  Display this help
+#   -v, --verbose               Enable Verbose Mode
+#   -V, --version               Display versions
+#   -l, --log                   Set log file
+#   -l [file], --log=[file]     Set log file
+
+
