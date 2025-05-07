f1e6166ab6a35cba05e0869cd3191ced8dcd61fe
TJ <tj.anscombe@gmail.com>
2025-05-07 19:13:58 -0400

    Update download-lfs-scripts.sh

--- scripts/lfsdownloader/download-lfs-scripts.sh
+++ scripts/lfsdownloader/download-lfs-scripts.sh
@@ -35,16 +35,31 @@
 download_site="https://raw.githubusercontent.com/Neroko/LFS/refs/heads/master/wget-list"
 download_directory="lfs_test"
 
+# Check if files exist:
+rm                               \
+    --verbose                    \
+    .sudo_as_admin_successful    \
+    .wget-hsts                   \
+    version-check.sh
+
+# Check for file in directory
+#if [ -f "$download_site" ]
+
 # Check for directory and see if it exist:
 if [ ! -d "$download_directory" ]; then
     mkdir -p "$download_directory";
 fi
 
-# Check for file in directory
-#if [ -f "$download_site" ]
-
 # Download files to directory
 wget                                        \
+    -N                                      \
+    -O "version-check.sh"                   \
     --timestamping                          \
     --directory-prefix="$download_directory"\
     "$download_site"
+
+# Set file permission:
+chmod                               \
+    --verbose                       \
+    755                             \
+    "version-check.sh"
