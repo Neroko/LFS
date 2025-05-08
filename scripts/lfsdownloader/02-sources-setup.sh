bbaea5f4430475095c4d83b97f27d6c5277b82cd
TJ <tj.anscombe@gmail.com>
2025-05-07 21:15:37 -0400

    Update 02-sources-setup.sh

--- scripts/lfsdownloader/02-sources-setup.sh
+++ scripts/lfsdownloader/02-sources-setup.sh
@@ -633,9 +633,9 @@ useradd                 \
 passwd lfs
 
 # Grant 'lfs' full access to all directories under '$LFS' by making 'lfs' the owner:
-chown -v lfs $LFS/{usr{,/*},lib,var,etc,bin,sbin,tools}
-case $(uname -m) in
-    x86_64) chown -v lfs $LFS/lib64 ;;
+chown --verbose lfs $LFS/{usr{,/*},lib,var,etc,bin,sbin,tools}
+case $(uname --machine) in
+    x86_64) chown --verbose lfs $LFS/lib64 ;;
 esac
 
 # == Note ==
