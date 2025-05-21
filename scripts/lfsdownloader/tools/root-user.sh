93e0443a09c124ef3f6df18ce233ee79a64ed629
TJ <tj.anscombe@gmail.com>
2025-05-21 13:38:05 -0400

    modified:   scripts/lfsdownloader/tools/root-user.sh

--- scripts/lfsdownloader/tools/root-user.sh
+++ scripts/lfsdownloader/tools/root-user.sh
@@ -1,8 +1,31 @@
 #!/bin/bash
 
+display_title="== Linux From Scratch (LFS) Root Check =="
+#
+# VERSION (LFS):
+current_version="12.3"
+#
+# VERSION (SCRIPT):
+script_version="1.0.0.0"
+#
+# DATE LAST EDITED:
+#   05/21/2025
+#
+# DATE CREATED:
+#   05/21/2025
+#
+# AUTHOR:
+#   TerryJohn Anscombe
+#
+# USAGE:
+#   root-check.sh
+#
+# DESCRIPTION
+#   Script to check if running as root user.
+
 if [[ "$EUID" -eq 0 ]]; then
     echo "Script is running as root"
 else
     echo "Script is not running as root";
-    exit
+    exit 1
 fi
