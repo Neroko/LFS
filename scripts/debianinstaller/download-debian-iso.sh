796e43fe9099ab3f59aaa7853a07dda9d66387c8
TJ <tj.anscombe@gmail.com>
2025-06-19 06:39:00 -0400

    modified:   scripts/debianinstaller/download-debian-iso.sh

---
+++ scripts/debianinstaller/download-debian-iso.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+debian-version-link='https://www.debian.org/CD/'
+
+debian-download-link='https://cdimage.debian.org/debian-cd/'
+
+debian-netinst-link="$debian-download-link/amd64/iso-cd/"
+debian-netinst-filename='debian-12.11.0-amd64-netinst.iso'
+
+debian-dvd-link='https://cdimage.debian.org/debian-cd/current/amd64/iso-dvd/'
+debian-dvd-filename='debian-12.11.0-amd64-DVD-1.iso'
+
+debian-live-link=''
